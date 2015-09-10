4fc79730fb54ff3c1f38d4efda7f8efa91923263
NatoBoram <nato.boram@gmail.com>
2015-09-10 12:21:02 -0400

    First push - test

---
+++ include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+	<hr />
+	<h1>Special Thanks</h1>
+	<ul>
+		<li><a href="http://forum.botoflegends.com/topic/71447-">ikitaBot</a></li>
+		<li><a href="http://motherfuckingwebsite.com/">Motherfucking Website</a></li>
+		<li><a href="http://bettermotherfuckingwebsite.com/">Better Motherfucking Website</a></li>
+		<li><a href="http://rafael.adm.br/css_browser_selector/">CSS Browser Selector</a></li>
+	</ul>
+</footer>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Some Secrets</title>
+		<link rel="stylesheet" href="style.css" />
+		<script src="http://github.com/rafaelp/css_browser_selector/raw/master/css_browser_selector.js" type="text/javascript"></script>
+    </head>
+    <body>
+		<?php include("include/header.php"); ?>
+		
+		
+		<?php include("include/footer.php"); ?>
+    </body>
+</html>
