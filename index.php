02af86a8aa32032b109471a05ec81559387fe34b
NatoBoram <nato.boram@gmail.com>
2015-09-10 14:32:20 -0400

    Returned to PHP

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Some Secrets</title>
+		<link rel="stylesheet" href="style.css" />
+		<script src="http://github.com/rafaelp/css_browser_selector/raw/master/css_browser_selector.js" type="text/javascript"></script>
+    </head>
+    <body>
+		<?php include("include/header.html"); ?>
+		
+		<section>
+			<p>Well, nothing to see here.</p>
+		</section>
+		
+		<?php include("include/footer.html"); ?>
+    </body>
+</html>
